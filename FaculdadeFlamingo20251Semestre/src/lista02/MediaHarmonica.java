6288c002ebe81a23cab50a6f8dc012c9a5454d39
clicheria-pc <clicheria-pc@CLICHERIA>
2025-03-10 17:30:49 -0300

    Atualizado

--- FaculdadeFlamingo20251Semestre/src/lista02/MediaHarmonica.java
+++ FaculdadeFlamingo20251Semestre/src/lista02/MediaHarmonica.java
@@ -1,5 +1,22 @@
 package lista02;
 
 public class MediaHarmonica {
+	public static void main(String[]args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o 1 numero: ");
+		int numero01 = sc.nextInt();
+		System.out.println("Digite o 2 numero: ");
+		int numero02 = sc.nextInt();
+		System.out.println("Digite 0 3 numero: ");
+		int numero03 = sc.nextInt();
+		//calcular o MMC (Minimo, Multiplo Comum)
+		/*(3*6*9)=> 3*6*9=162*/
+		float mediaHarmonica =3* (numero01+numero02+numero03)/
+	
+		
+	}
+	
 
 }
