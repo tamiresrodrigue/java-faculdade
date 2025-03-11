37824c520f6d0ff9203fc78ec85d3642189a0409
clicheria-pc <clicheria-pc@CLICHERIA>
2025-03-11 15:35:54 -0300

    Merge branch 'master' of https://github.com/tamiresrodrigue/java-faculdade.git

--- FaculdadeFlamingo20251Semestre/src/lista02/MediaGeometrica.java
+++ FaculdadeFlamingo20251Semestre/src/lista02/MediaGeometrica.java
@@ -2,21 +2,40 @@ package lista02;
 
 import java.util.Scanner;
 
-public class MediaGeometrica {
-	public static void main(String[]args){
+public class MediaGemometrica {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		System.out.print("Digite a quantidade de numeros: ");
+		int numero1 = sc.nextInt();
+		
+		
+		double somaProduto = 1.0;
+		
+		double[] numeros = new double[numero1];
+		
+		
+		for (int i = 0; i <numero1; i++) {
+			System.out.print("Digite o número " + (i + 1) + ": ");
+			numeros[i] = sc.nextDouble();
+			somaProduto *= numeros[i];
+			
+		}
+		
+		double somaGeometrica = Math.pow(somaProduto, 1.0 / numero1);
+		
+		
+		System.out.printf("A media Geometrica dos números fornecidos é: %.2f%n", somaGeometrica);
+		
+		
+		sc.close();
+	}
 
-	Scanner sc = new Scanner(System.in);{
-		System.out.println("Digite o 1 numero: ");
-		double numero01 = sc.nextDouble();
-		System.out.println("Digite o 2 numero: ");
-		double numero02 = sc.nextDouble();
-		System.out.println("Digite 0 3 numero: ");
-		double numero03 = sc.nextDouble();
-		//calcular o MMC (Minimo, Multiplo Comum)
-		/*(3*6*9)=> 3*6*9=162*/
-		double mediaGeometrica = Math.cbrt(numero01*numero02*numero03);
-				System.out.println("A média geometrica dos numeros digitados é:" +mediaGeometrica);
-	}
-	}
+}
 
 	
--- FaculdadeFlamingo20251Semestre/src/lista02/MediaPonderada.java
+++ FaculdadeFlamingo20251Semestre/src/lista02/MediaPonderada.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class MediaPonderada {
 
-	public static void main(String[] args) {
+	public static void main(String[]args) {
 		
 		Scanner sc = new Scanner(System.in);
 		
