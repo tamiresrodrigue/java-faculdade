0c47bc1221ea8d113633c05425135a5494ddd66b
clicheria-pc <clicheria-pc@CLICHERIA>
2025-03-11 15:23:49 -0300

    projetos atualizados

--- FaculdadeFlamingo20251Semestre/src/lista02/MediaPonderada.java
+++ FaculdadeFlamingo20251Semestre/src/lista02/MediaPonderada.java
@@ -3,23 +3,39 @@ package lista02;
 import java.util.Scanner;
 
 public class MediaPonderada {
-	public static void main(String[]args) {
+
+	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Digite o valor da sua 1° Nota:");
-		float nota1= sc.nextFloat();
-		System.out.println("Digite o valor do peso da sua 1°Nota");
-		int peso1 = sc.nextInt();
-		System.out.println("Digite o valor da sua 2° Nota:");
-		float nota2= sc.nextFloat();
-		System.out.println("Digite o valor do peso da sua 2°Nota");
-		int peso2 = sc.nextInt();
-		System.out.println("Digite o valor da sua 3° Nota:");
-		float nota3= sc.nextFloat();
-		System.out.println("Digite o valor do peso da sua 3°Nota");
-		int peso3 = sc.nextInt();
-		sc.close();
+		System.out.println("Digite a primeira nota");
 		
+		double nota1 = sc.nextDouble();
+		
+		System.out.println("Digite o primeiro peso");
+		
+		double peso1 = sc.nextDouble();
+		
+		System.out.println("Digite a segunda nota");
+		
+		double nota2 = sc.nextDouble();
+		
+		System.out.println("Digite o segundo peso");
+		
+		double peso2 = sc.nextDouble();
+		
+		System.out.println("Digite a terceira nota");
+		
+		double nota3 = sc.nextDouble();
+		
+		System.out.println("Digite o tericeiro peso");
+		
+		double peso3 = sc.nextDouble();
+				
+		double mediaPonderada = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3) / (peso1 + peso2 + peso3);
+		
+		System.out.println("Digite suas notas: " + mediaPonderada);
+		
+		sc.close();
 	}
-
 }
